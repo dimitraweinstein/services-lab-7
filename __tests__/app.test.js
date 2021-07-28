3d9599dfab221e4ed8264e47fb910da0ebe5ad27
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-28 16:12:38 -0700

    created first test route to create student, currently red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,21 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('futurama routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a student via POST', async () => {
+    const student = { firstName: 'Fawn', lastName: 'Nioso', status: 'active' };
+
+    const res = await request(app).post('/students').send(student);
+
+    expect(res.body).toEqual({
+      id: '1',
+      firstName: 'Fawn',
+      lastName: 'Nioso',
+      status: 'active'
+    });
   });
 });
