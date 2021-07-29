ea275d3681f133ae204817b63c733e32b6e36e51
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-28 17:08:10 -0700

    created stest for get all students, currently red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,4 +21,32 @@ describe('student routes', () => {
       status: 'active'
     });
   });
+
+  it('gets all students via GET', async () => {
+    const fawn = await Student.insert({
+      id: '1',
+      firstName: 'Fawn',
+      lastName: 'Nioso',
+      status: 'active'
+    });
+
+    const jon = await Student.insert({
+      id: '2',
+      firstName: 'Jon',
+      lastName: 'Stuart',
+      status: 'active'
+    });
+
+    const alexander = await Student.insert({
+      id: '3',
+      firstName: 'Alexander',
+      lastName: 'Spycher',
+      status: 'active'
+    });
+
+    return request(app).get('/api/v1/students')
+      .then((res) => {
+        expect(res.body).toEqual([fawn, jon, alexander]);
+      });
+  });
 });
